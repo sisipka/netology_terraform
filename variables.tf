e43684fff9b9523f4b04d3284c7d820e20bd5cbb
sisipka <96078556+sisipka@users.noreply.github.com>
2022-06-20 16:15:24 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g3va6uroak1bss6lnp"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1ge911130k5mdbil616"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8jvafqbrslq4pa1tje"
+}
